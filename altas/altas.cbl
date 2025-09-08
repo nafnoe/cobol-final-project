b7f5cd9f7259d4f5846b41f5377c53f9946540c7
nafnoe <noeliaarodas@gmail.com>
2025-09-08 18:26:51 -0300

    subimos nuestro projecto

--- altas/altas.cbl
+++ altas/altas.cbl
@@ -1,12 +1,44 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. ALTA-CLIENTES.
-       AUTHOR. YOUR-NAME.
-       DATE-WRITTEN. 2025-08-05.
-       
+       PROGRAM-ID. MAIN.
+       ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+
+       01 MENU-CHOICE-STR PIC X(2).
+       01 MENU-CHOICE     PIC 9.
+          88 CREATE-CBF   VALUE 1.
+          88 EXIT-PROGRAM VALUE 2.
+
+       01 ID-CUENTA.
+           05 ID-D1   PIC 9.
+           05 ID-D2   PIC 9.
+           05 ID-D3   PIC 9.
+           05 ID-D4   PIC 9.
+           05 ID-D5   PIC 9.
+
+       01  CLAVE-BANCARIA PIC X(6).
+
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-           DISPLAY "ESTE ES EL PROGRAMA DE ALTAS"
-           STOP RUN.
-           
+       MAIN-PROGRAM.
+               PERFORM DISPLAY-MENU UNTIL EXIT-PROGRAM.
+            STOP RUN.
+
+        DISPLAY-MENU.
+           DISPLAY "-------------------------------------------------".
+           DISPLAY "---PROGRAMA CALCULO DE CLAVE BANCARIA FICTICIA---".
+           DISPLAY "-------------------------------------------------".
+           DISPLAY "1. Crear claves bancarias desde CSV".
+           DISPLAY "2. Salir".
+           DISPLAY "SU OPCION: " WITH NO ADVANCING.
+           ACCEPT MENU-CHOICE-STR.
+           MOVE FUNCTION NUMVAL(MENU-CHOICE-STR) TO MENU-CHOICE.
+
+           EVALUATE TRUE
+              WHEN CREATE-CBF
+                  CALL "CREAR-CBF" USING ID-CUENTA, CLAVE-BANCARIA
+              WHEN EXIT-PROGRAM
+                   CONTINUE
+              WHEN OTHER
+                  DISPLAY "OPCION INVALIDA"
+           END-EVALUATE.
+
